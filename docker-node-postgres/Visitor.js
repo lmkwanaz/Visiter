3c5a346089c2328f566e116377c25af765d5dfb2
lwandile <lwandile.mk3@gmail.com>
2019-12-11 13:22:10 +0200

    added update,deleteVisiter,viewOnevisiter, deleteAllVisiters

--- docker-node-postgres/Visitor.js
+++ docker-node-postgres/Visitor.js
@@ -65,8 +65,29 @@ const addNewVisitor = (Data) =>{
 // assistant: 'neo',
 // comments: 'up to so far he is a good person'});
 
+// addNewVisitor({name: 'phumlani',
+// age: 17,
+// date: '23-06-2010',
+// time: '18:00:00',
+// assistant: 'neo',
+// comments: 'up to so far he is a good person'});
 
-const seeTable = () =>{
+// addNewVisitor({name: 'tebza',
+// age: 24,
+// date: '25-06-2010',
+// time: '19:00:00',
+// assistant: 'neo',
+// comments: 'up to so far he is a bad person'});
+
+// addNewVisitor({name: 'kenneth',
+// age: 24,
+// date: '20-06-2012',
+// time: '22:00:00',
+// assistant: 'lwandile',
+// comments: 'up to so far he is a bad person'});
+
+
+const seeAllVisiters = () =>{
   pool.query('SELECT * FROM Visiters', (error, results)=>{
     if(error){
       throw error;
@@ -77,19 +98,72 @@ const seeTable = () =>{
   });
 }
 
-seeTable();
-// const helloWorld = () => {
-//   pool.query(
-//     "SELECT $1::text as message",
-//     ["Hello world!"],
-//     (error, results) => {
-//       if (error) {
-//         throw error;
-//       }
+//seeAllVisiters();
 
-//       console.log(results.rows);
-//     }
-//   );
-// };
 
-// helloWorld();
+const deleteVisiter = (Data) =>{
+  pool.query('DELETE FROM Visiters WHERE name=($1)', 
+  [Data.name], (error, results) =>{
+    if(error){
+      throw error;
+    }else{
+      console.log("the visiter has been deleted from the database");
+      seeAllVisiters();
+    }
+  });
+}
+
+//deleteVisiter({name: 'tebogo'})
+
+
+const updateVisiter = (Data) =>{
+  pool.query('UPDATE Visiters SET name=($1), age=($2), date=($3), time=($4), assistant=($5), comments=($6) WHERE id=($7)',
+  [Data.name, Data.age, Data.date, Data.time, Data.assistant, Data.comments, Data.id],
+  (error, results) =>{
+    if(error){
+      throw error;
+    }else{
+      console.log("the Visiters has been updated"+'\n');
+      seeAllVisiters();
+      
+    }
+  });
+}
+
+// updateVisiter({name: 'makhenzo',
+// age: 15,
+// date: '16-09-2019',
+// time: '18:00:00',
+// assistant: 'andy',
+// comments: 'up to so far he is a good person',
+// id: 5});
+
+const viewOneVisiter = (id) =>{
+  pool.query(`SELECT * FROM Visiters WHERE id= ${id}`,
+  (error, results) =>{
+    if(error){
+      throw error;
+    }else{
+      console.log("is this a Visiter you are looking for?"+"\n");
+      console.log(results.rows);
+    }
+    
+  });
+}
+
+//viewOneVisiter(5);
+
+
+const deleteAllVisiters = () =>{
+  pool.query('DELETE FROM Visiters',
+   (error, results) =>{
+     if(error){
+       throw error;
+     }else{
+       console.log(results);
+       
+     }
+   });
+}
+
+//deleteAllVisiters()
